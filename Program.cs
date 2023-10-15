9e001ffeb78add15e619b4d466fb5230d5bc89ae
Vlad-Eris <146423204+Vlad-Eris@users.noreply.github.com>
2023-10-15 23:34:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Impartire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+
+            x=Convert.ToInt32(Console.ReadLine());
+            y=Convert.ToInt32(Console.ReadLine());
+            z = x / y;
+
+            if (x != 0 && x % y == 0)
+            {
+                Console.WriteLine(z);
+            }
+            else
+            {
+                Console.WriteLine("Impartirea nu poate fi efectuata");
+            }
+        }
+    }
+}
